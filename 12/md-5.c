60272bb125ff34ad16e49060d1e79d4c88bd2cee
gbdngb12 <gbdngb12@naver.com>
2022-12-25 17:42:54 +0900

    add public key encrypton

---
+++ 12/md-5.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_BLOCK 64  // 해쉬 블록 크기(512비트)(byte)
+#define HASH_DATA 16   // 해쉬 출력 값의 크기(byte)
+
+// 순환이동 횟수
+#define S11 7
+#define S12 12
+#define S13 17
+#define S14 22
+#define S21 5
+#define S22 9
+#define S23 14
+#define S24 20
+#define S31 4
+#define S32 11
+#define S33 16
+#define S34 23
+#define S41 6
+#define S42 10
+#define S43 15
+#define S44 21
+
+// 타입 정의
+typedef unsigned char byte;
+typedef unsigned int uint;
+typedef unsigned long long uint64;
+
+static int isAddpad = 0;
+static uint init_reg[4];        // 초기 레지스터
+static byte digest[HASH_DATA];  // 해쉬 값
+
+//기약 논리함수
+#define F(X,y,z) ( ((X)&(y)) | ((~X)&(z)) )
+#define G(X,y,z) ( ((X)&(z)) | ((y)&(~(z))) )
+#define H(X,y,z) ( ((X)^(y)^(z)) )
+#define I(X,y,z) ( (y) ^ ((X)|(~(z))) )
+
+static void padding(byte *, uint64);
+static void md_5_init();
+static void md_5_digest(byte *);
+static void md_5(FILE *, byte *);
+static void ff(uint* a,uint b, uint c, uint d, uint _m, int s, uint t);
+static void gg(uint* a,uint b, uint c, uint d, uint _m, int s, uint t);
+static void hh(uint* a,uint b, uint c, uint d, uint _m, int s, uint t);
+static void ii(uint* a,uint b, uint c, uint d, uint _m, int s, uint t);
+
+#define byte_to_word(a,b,c,d) ( ((a << 24) + (b << 16) + (c << 8) + d) )
+#define circular_shift(X,n) ( ((X) << n) | ((X) >> (32-n)) )			
+
+static void make_bit_128(byte[16],uint , uint , uint , uint);//word단위 해쉬 값을 byte단위로 변환 하는 함수
+
+void ff(uint* a,uint b, uint c, uint d, uint _m, int s, uint t) {
+    *a = b + circular_shift((*a + F(b,c,d) + _m + t), s);
+}
+
+void gg(uint* a,uint b, uint c, uint d, uint _m, int s, uint t) {
+    *a = b + circular_shift((*a + G(b,c,d) + _m + t), s);
+}
+
+void hh(uint* a,uint b, uint c, uint d, uint _m, int s, uint t) {
+    *a = b + circular_shift((*a + H(b,c,d) + _m + t), s);
+}
+
+void ii(uint* a,uint b, uint c, uint d, uint _m, int s, uint t) {
+    *a = b + circular_shift((*a + I(b,c,d) + _m + t), s);
+}
+
+void padding(byte *in, uint64 msg_len) {
+    int i;
+    byte *ptr = (byte *)&msg_len;
+
+    if ((msg_len % HASH_BLOCK) < 56) {    // 56바이트 보다 작다면
+        in[msg_len % HASH_BLOCK] = 0x80;  // 메시지의 끝 한비트 뒤에 1을 적음
+        msg_len *= 8;                     // 메시지의 길이값(바이트)를 비트로 변환 (16byte -> 128bit)
+
+        for (i = 0; i < 8; i++) {
+            in[HASH_BLOCK - i - 1] = *(ptr + (7 - i));  //msg_len값이 little-endian일 경우!  8byte Data -> 8byte Array로 저장함
+        }
+    } else {
+        in[msg_len % HASH_BLOCK] = 0x80;
+        msg_len *= 8;
+        isAddpad = 1;
+        for (i = 0; i < 8; i++) {
+            in[HASH_BLOCK * 2 - i - 1] = *(ptr + (7 - i));  // little endian방식으로 저장함
+        }
+    }
+}
+
+// little endian 컴퓨터 이므로 순서를 정확히 지켜야함
+void md_5_init() {
+    init_reg[0] = 0x67452301;  // 실제 컴퓨터에 저장 순서 0x01234567
+    init_reg[1] = 0xefcdab89;  // 0x89abcdef
+    init_reg[2] = 0x98badcfe;  // 0xfedcba98
+    init_reg[3] = 0x10325476;  // 0x76543210
+}
+
+void md_5_digest(byte *in) {
+    int i;
+    uint a,b,c,d;
+    uint x[16] = { 0 };//중간과정에서 처리되는 32비트 * 4 => 128비트 값
+    
+    for(i = 0; i < 64; i += 4) {//message -> x로 저장
+        x[i/4] = byte_to_word(in[i + 3], in[i + 2], in[i + 1], in[i]);//save to little-endian
+    }
+
+    a = init_reg[0]; b = init_reg[1]; c = init_reg[2]; d = init_reg[3];
+
+    /*Round 1*/
+    ff(&a, b, c, d, x[ 0], S11, 0xd76aa478); /* 1회*/
+	ff(&d, a, b, c, x[ 1], S12, 0xe8c7b756); /* 2회*/
+	ff(&c, d, a, b, x[ 2], S13, 0x242070db); /* 3회*/
+	ff(&b, c, d, a, x[ 3], S14, 0xc1bdceee); /* 4회*/
+	ff(&a, b, c, d, x[ 4], S11, 0xf57c0faf); /* 5회*/
+	ff(&d, a, b, c, x[ 5], S12, 0x4787c62a); /* 6회*/
+	ff(&c, d, a, b, x[ 6], S13, 0xa8304613); /* 7회*/
+	ff(&b, c, d, a, x[ 7], S14, 0xfd469501); /* 8회*/
+	ff(&a, b, c, d, x[ 8], S11, 0x698098d8); /* 9회*/
+	ff(&d, a, b, c, x[ 9], S12, 0x8b44f7af); /* 10회 */
+	ff(&c, d, a, b, x[10], S13, 0xffff5bb1); /* 11회 */
+	ff(&b, c, d, a, x[11], S14, 0x895cd7be); /* 12회 */
+	ff(&a, b, c, d, x[12], S11, 0x6b901122); /* 13회 */
+	ff(&d, a, b, c, x[13], S12, 0xfd987193); /* 14회 */
+	ff(&c, d, a, b, x[14], S13, 0xa679438e); /* 15회 */
+	ff(&b, c, d, a, x[15], S14, 0x49b40821); /* 16회 */
+
+    /*Round 2*/
+    gg(&a, b, c, d, x[ 1], S21, 0xf61e2562); /* 17회 */
+	gg(&d, a, b, c, x[ 6], S22, 0xc040b340); /* 18회 */
+	gg(&c, d, a, b, x[11], S23, 0x265e5a51); /* 19회 */
+	gg(&b, c, d, a, x[ 0], S24, 0xe9b6c7aa); /* 20회 */
+	gg(&a, b, c, d, x[ 5], S21, 0xd62f105d); /* 21회 */
+	gg(&d, a, b, c, x[10], S22,  0x2441453); /* 22회 */
+	gg(&c, d, a, b, x[15], S23, 0xd8a1e681); /* 23회 */
+	gg(&b, c, d, a, x[ 4], S24, 0xe7d3fbc8); /* 24회 */
+	gg(&a, b, c, d, x[ 9], S21, 0x21e1cde6); /* 25회 */
+	gg(&d, a, b, c, x[14], S22, 0xc33707d6); /* 26회 */
+	gg(&c, d, a, b, x[ 3], S23, 0xf4d50d87); /* 27회 */
+	gg(&b, c, d, a, x[ 8], S24, 0x455a14ed); /* 28회 */
+	gg(&a, b, c, d, x[13], S21, 0xa9e3e905); /* 29회 */
+	gg(&d, a, b, c, x[ 2], S22, 0xfcefa3f8); /* 30회 */
+	gg(&c, d, a, b, x[ 7], S23, 0x676f02d9); /* 31회 */
+	gg(&b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32회 */
+
+    /*Round 3*/
+	hh(&a, b, c, d, x[ 5], S31, 0xfffa3942); /* 33 */
+	hh(&d, a, b, c, x[ 8], S32, 0x8771f681); /* 34 */
+	hh(&c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
+	hh(&b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
+	hh(&a, b, c, d, x[ 1], S31, 0xa4beea44); /* 37 */
+	hh(&d, a, b, c, x[ 4], S32, 0x4bdecfa9); /* 38 */
+	hh(&c, d, a, b, x[ 7], S33, 0xf6bb4b60); /* 39 */
+	hh(&b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
+	hh(&a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
+	hh(&d, a, b, c, x[ 0], S32, 0xeaa127fa); /* 42 */
+	hh(&c, d, a, b, x[ 3], S33, 0xd4ef3085); /* 43 */
+	hh(&b, c, d, a, x[ 6], S34,  0x4881d05); /* 44 */
+	hh(&a, b, c, d, x[ 9], S31, 0xd9d4d039); /* 45 */
+	hh(&d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
+	hh(&c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
+	hh(&b, c, d, a, x[ 2], S34, 0xc4ac5665); /* 48 */
+	
+	/*Round 4*/
+	ii(&a, b, c, d, x[ 0], S41, 0xf4292244); /* 49 */
+	ii(&d, a, b, c, x[ 7], S42, 0x432aff97); /* 50 */
+	ii(&c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
+	ii(&b, c, d, a, x[ 5], S44, 0xfc93a039); /* 52 */
+	ii(&a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
+	ii(&d, a, b, c, x[ 3], S42, 0x8f0ccc92); /* 54 */
+	ii(&c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
+	ii(&b, c, d, a, x[ 1], S44, 0x85845dd1); /* 56 */
+	ii(&a, b, c, d, x[ 8], S41, 0x6fa87e4f); /* 57 */
+	ii(&d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
+	ii(&c, d, a, b, x[ 6], S43, 0xa3014314); /* 59 */
+	ii(&b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
+	ii(&a, b, c, d, x[ 4], S41, 0xf7537e82); /* 61 */
+	ii(&d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
+	ii(&c, d, a, b, x[ 2], S43, 0x2ad7d2bb); /* 63 */
+	ii(&b, c, d, a, x[ 9], S44, 0xeb86d391); /* HASH_BLOCK */
+
+    init_reg[0] += a;
+    init_reg[1] += b;
+    init_reg[2] += c;
+    init_reg[3] += d;
+    
+    make_bit_128(digest, init_reg[0], init_reg[1], init_reg[2], init_reg[3]);
+}
+
+void make_bit_128(byte in[16],uint a, uint b, uint c, uint d) { //little-endian 으로 다루었으므로 그대로저장
+    int i;
+
+	for(i=0;i<16;i++)
+	{
+		if(i < 4)
+			in[i] = ((a & ((uint)0x000000FF << (i*8))) >> (i*8));
+		else if(i < 8)
+			in[i] = ((b & ((uint)0x000000FF << ((i%4)*8))) >> ((i%4)*8));
+		else if(i < 12)
+			in[i] = ((c & ((uint)0x000000Ff << ((i%4)*8))) >> ((i%4)*8));
+		else
+			in[i] = ((d & ((uint)0x000000FF << ((i%4)*8))) >> ((i%4)*8));
+	}
+}
+
+void md_5(FILE *fptr, byte *result) {
+    int i, size = 0;
+    byte msg[HASH_BLOCK * 2] = { 0 };//추가될수도 있으므로 두개의 블록을 할당한다.
+    uint64 f_size = 0;
+
+    md_5_init();
+
+    while((size = fread(msg, sizeof(byte), HASH_BLOCK, fptr))) {
+        f_size += size;
+
+        if(size < HASH_BLOCK) {
+            padding(msg, f_size);
+        }
+
+        md_5_digest(msg);
+        if(isAddpad) md_5_digest(msg+HASH_BLOCK);//블록이 추가되었다면 추가된 블록도 작업 수행
+        memset(msg, 0, HASH_BLOCK * 2);//다음 해쉬를 위해 메모리 초기화
+    }
+    for(i = 0; i < HASH_DATA; i++) {
+        result[i] = digest[i];//해쉬 결과 값 저장
+    }
+}
---
+++ 12/rsa_enc_dec.c
@@ -0,0 +1,441 @@
+#include "rsa_enc_dec.h"
+
+
+void rsa_encryption(unsigned char* plain_text, unsigned char* result) {
+    int i, count = 0;
+    short check = 1;
+    FILE* fptr;
+
+    // 수신자의 공개키 파일을 연다.
+    if ((fptr = fopen("public_key.txt", "rb")) == NULL) {
+        printf("file open failed!!\n");
+        exit(1);
+    }
+
+    // 파일로부터 공개키 e와 n을 저장한다.
+    for (i = mb - 1; i >= 0; i--) {
+        fscanf(fptr, "%I64x ", &N[i]);  // 공개키 N
+    }
+    for (i = mb - 1; i >= 0; i--) {
+        fscanf(fptr, "%I64x ", &E[i]);  // 공개키 e
+    }
+
+    fclose(fptr);
+    // 평문을 모두 암호화 할때 까지
+    // 117바이트씩 암호를 수행한다.(11 바이트 = 패딩)
+    while (check == 1) {
+        // 평문을 읽어 이진 형태로 저장한다.
+        check = get_from_message(plain_text + count * DATA_LEN, h, DATA_LEN);
+
+        //
+        if (check != -1) {
+            convert_binary_to_oct(h, DATA, DATA_LEN);  // 이진 평문을 octet으로 변환
+
+            // OAEP 암호문 블록 패딩 (EB1 <- [00|02|PS|00|DATA])
+            rand_generator(ps, LEN_PS * 8);           // 패딩 스트링으로 사용할 랜덤 수 생성
+            convert_binary_to_oct(ps, O_PS, LEN_PS);  // 생성한 이진 랜덤수를 octet으로 변환
+
+            EB[mb * 4 - 1] = 0x00;
+            EB[mb * 4 - 2] = 0x02;
+
+            // PS padding
+            for (i = mb * 4 - 3; i > DATA_LEN; i--) {
+                EB[i] = O_PS[i - DATA_LEN - 1];
+            }
+
+            EB[DATA_LEN] = 0x00;
+
+            // data
+            for (i = DATA_LEN - 1; i >= 0; i--) {
+                EB[i] = DATA[i];
+            }
+
+            for (i = mb * 4 - 1; i >= 0; i -= 4) {
+                EB1[i / 4] = (EB[i] << (DHEX - OCT)) + (EB[i - 1] << (OCT + OCT)) + (EB[i - 2] << OCT) + EB[i - 3];
+            }
+            // 암호문 블록 패딩 종료
+
+            // C = M^e mod N (M - bit)
+            left_to_right_pow(EB1, E, S, N, mb);  // 수신자의 공개키로 암호화
+
+            // radix 형태의 암호문을 이진 형태로 변환
+            convert_radix_to_binary(S, s, mb);
+
+            // 이진 형태의 암호문을 바이트 형태로 변화하여 저장
+            put_to_message(result + count * B_S, s, B_S);
+
+            count++;
+        }
+    }
+}
+void rsa_decryption(unsigned char* cipher_text, unsigned char* result) {
+    int i, count = 0;
+    short check = 1;
+    FILE* fptr;
+
+    // 사용자의 비밀키 파일을 연다
+    if ((fptr = fopen("secret_key.txt", "rb")) == NULL) {
+        printf("file open failed!!\n");
+        exit(1);
+    }
+
+    // 파일로부터 공개키 d와 모듈러 n을 저장한다
+    for (i = mb - 1; i >= 0; i--) fscanf(fptr, "%I64x ", &N[i]);
+    for (i = mb - 1; i >= 0; i--) fscanf(fptr, "%I64x ", &D[i]);
+
+    fclose(fptr);
+
+    // 암호문을 모두 암호화 할 때까지
+    // 128 바이트씩 암호를 수행한다(11 바이트 = 패딩 포함)
+    while (check == 1) {
+        // 암호문을 읽어 이진 형태로 저장한다
+        check = get_from_message(cipher_text + count * B_S, s, B_S);
+
+        if (check != -1) {
+            convert_binary_to_radix(s, S, mb);  // 이진 형태의 암호문을 Radix로 변환
+
+            /*** M = C^d mod N (M-bit) ***/
+            left_to_right_pow(S, D, H, N, mb);  // 사용자의 비밀키로 복호화
+
+            convert_radix_to_binary(H, v_h, mb);       // 복호화된 데이터를 이진 형태로 변환
+            convert_binary_to_oct(v_h, D_EB, mb * 4);  // 이진 형태의 데이터를 octet으로 변환
+
+            // 패딩을 제외한 복호문을 추출한다
+            for (i = DATA_LEN - 1; i >= 0; i--)
+                D_DATA[i] = D_EB[i];
+
+            // 추출한 복호문을 이진 형태로 변환
+            convert_oct_to_binary(D_DATA, d_d, DATA_LEN);
+            // 이진 형태의 복호문을 바이트 형태로 저장한다
+            put_to_message(result + count * DATA_LEN, d_d, DATA_LEN);
+
+            count++;
+        }
+    }
+}
+// 메시지를 읽어 이진 형태로 저장
+int get_from_message(unsigned char* msg, short* a, short mn) {
+    register i, j;
+    short flag = 1, cnt = 0, mm;
+    unsigned char b[m / Char_NUM] = {
+        0,
+    };
+
+    mm = mn * Char_NUM;
+
+    for (i = 0; i < mm; i++)
+        a[i] = 0;
+
+    // 메시지 버퍼에서 한 바이트씩 읽는다
+    for (i = 0; i < mn; i++) {
+        if (msg[i] == '\0') {
+            if (i == 0)
+                return -1;
+
+            if (mn < B_S) {
+                flag = 0;
+                break;
+            }
+        }
+
+        b[i] = msg[i];
+    }
+
+    cnt = 0;
+    // 바이트 단위의 데이터를 이진 형태로 변환
+    for (i = mn - 1; i >= 0; i--) {
+        for (j = 0; j < Char_NUM; j++) {
+            a[cnt++] = (b[i] >> j) & 0x01;
+        }
+    }
+
+    return (flag);
+}
+
+// 이진 형태의 데이터를 바이트 형태로 저장
+void put_to_message(unsigned char* msg, short* a, short mn) {
+    register i, j;
+    short cnt = 0;
+    unsigned char b[m / Char_NUM] = {
+        0,
+    };
+    unsigned char mask[Char_NUM] = {0x01, 0x02, 0x04, 0x08,
+                                    0x10, 0x20, 0x40, 0x80};
+
+    cnt = 0;
+    // 이진 형태의 데이터를 바이트 형태로 변환한다
+    for (i = mn - 1; i >= 0; i--) {
+        for (j = 0; j < Char_NUM; j++) {
+            b[i] = b[i] + a[cnt++] * mask[j];
+        }
+    }
+    // 변환한 데이터를 메시지 버퍼에 저장한다
+    for (i = 0; i < mn; i++)
+        msg[i] = b[i];
+}
+
+void convert_binary_to_radix(short *A, int64 *B, short mn) {
+    register i, j, k;
+
+    for (i = mn - 1; i >= 0; i--) B[i] = 0x00;
+
+    i = mn * DHEX - 1;
+    for (k = 0; k <= mn - 1; k++) {
+        B[k] = 0x00;
+        for (j = DHEX - 1; j >= 0; j--) {
+            B[k] += A[i--] * mask[j];
+            if (i < 0) break;
+        }
+        if (i < 0) break;
+    }
+}
+
+/********************************************************************/
+/***********   Function name :  convert_radix_to_binary (A,b,mn)       **********/
+/***********   Description   :  convert radix into bin.    **********/
+/********************************************************************/
+void convert_radix_to_binary(int64 *A, short *B, short mn) {
+    register i, j, k;
+
+    for (i = 0; i < mn * DHEX; i++) B[i] = 0;
+    k = mn * DHEX - 1;
+    for (i = 0; i <= mn - 1; i++) {
+        for (j = 0; j <= DHEX - 1; j++) {
+            B[k--] = (A[i] >> j) & 0x01;
+            if (k < 0) break;
+        }
+        if (k < 0) break;
+    }
+}
+
+/********************************************************************/
+/***********   Function name :  rand_generator (a,n)               **********/
+/***********   Description   : n-bits random               **********/
+/***********                   number generator.           **********/
+/********************************************************************/
+void rand_generator(short *out, short n) {
+    register j, k;
+    short x;
+    long t;
+
+    srand((unsigned)(time(NULL)));
+    // delay(100);
+
+    j = 0;
+    while (1) {
+        x = rand();
+        for (k = 0; k < 15; k++) {
+            out[n - 1 - j] = (x >> k) & 0x01;
+            j++;
+            if (j >= n) return;
+        }
+    }
+}
+
+/********************************************************************/
+/*****     Function name : modular(C, N mn)                     *****/
+/*****     Description   : C = C mod N                          *****/
+/********************************************************************/
+void modular(int64 *X, int64 *N, short mn) {
+    register i, j, k;
+    short shift, posit;
+    int64 arryA[2 * mb + 1] = {
+        0,
+    },
+                         arryN[2 * mb + 1] = {
+                             0,
+                         };
+    int64 acumA, acumB, acumN, acumQ;
+    int32 acumC;
+
+    acumN = N[mn - 1] + 0x01;
+
+    while (1) {
+        for (k = 2 * mn - 1; k >= 0; k--)
+            if (X[k] > 0x00)
+                break;
+        if (k <= mn - 1)
+            break;
+
+        acumA = X[k] * rdx + X[k - 1];
+        acumQ = acumA / acumN;
+
+        if (acumQ > (rdx - 1))
+            acumQ = rdx - 1;
+
+        shift = k - mn; /**  shift number **/
+
+        acumC = 0x00;
+        for (k = 0; k <= mn - 1; k++) {
+            acumA = N[k] * acumQ + acumC;
+            acumC = acumA >> DHEX;
+            acumA = acumA & LAND;
+            j = k + shift;
+            if (X[j] < acumA) {
+                X[j] += rdx;
+                posit = j;
+                while ((X[j + 1]) == 0 && (j < (mn + shift))) {
+                    X[j + 1] += rdx - 1;
+                    j++;
+                }
+                X[j + 1] -= 0x01;
+                j = posit;
+            }
+            X[j] = (X[j] - acumA) & LAND;
+        }
+        X[mn + shift] = X[mn + shift] - acumC;
+    }
+
+    while (1) {
+        for (i = mn - 1; i >= 0; i--) {
+            if ((X[i] & LAND) != (N[i] & LAND)) {
+                if ((X[i] & LAND) > (N[i] & LAND))
+                    break;
+                else
+                    return (0);
+            }
+        }
+
+        acumA = X[mn - 1];
+        acumA = acumA / acumN;
+
+        if (acumA == 0x00) {
+            for (i = 0; i <= mn - 1; i++) {
+                if (X[i] < N[i]) {
+                    X[i] += rdx;
+                    posit = i;
+                    while ((X[i + 1] == 0) && (i < mn)) {
+                        X[i + 1] += rdx - 1;
+                        i++;
+                    }
+                    X[i + 1] -= 0x01;
+                    i = posit;
+                }
+                X[i] = (X[i] - N[i]) & LAND;
+            }
+        }
+
+        else {
+            acumC = 0x00;
+            for (i = 0; i <= mn - 1; i++) {
+                acumB = N[i] * acumA + acumC;
+                acumC = acumB >> DHEX;
+                acumB = acumB & LAND;
+                if (X[i] < acumB) {
+                    X[i] += rdx;
+                    posit = i;
+                    while ((X[i + 1] == 0) && (i < mn)) {
+                        X[i + 1] += rdx - 1;
+                        i++;
+                    }
+                    X[i + 1] -= 0x01;
+                    i = posit;
+                }
+                X[i] = (X[i] - acumB) & LAND;
+            }
+        }
+    }
+}
+int main () {
+    
+}
+
+/********************************************************************/
+/*****     Function name : convert_mma(A,B,C,N,mn) (Conventional)  *****/
+/*****     Description   : C= A*B mod N                         *****/
+/********************************************************************/
+void convert_mma(int64 *A, int64 *B, int64 *C, int64 *N, short mn) {
+    register i, j, k;
+    int64 arryC[mb * 2], X[mb * 2]; /** temporary arrys **/
+    int64 acumA;                    /** temporary acumulators **/
+    int32 acumC;
+
+    for (k = 2 * mn - 1; k >= 0; k--) arryC[k] = 0x00;
+
+    for (i = 0; i <= mn - 1; i++) {
+        if (A[i] > 0x00) {
+            acumC = 0x00;
+            for (j = 0; j <= mn - 1; j++) {
+                acumA = A[i] * B[j] + arryC[i + j] + acumC;
+                arryC[i + j] = acumA & LAND;
+                acumC = acumA >> DHEX;
+            }
+            arryC[i + j] = acumC;
+        }
+    }
+
+    for (i = 2 * mn - 1; i >= 0; i--)
+        X[i] = arryC[i];
+
+    modular(X, N, mn);
+
+    for (i = 0; i <= mn - 1; i++)
+        C[i] = X[i];
+}
+
+/********************************************************************/
+/***********   Function name :  convert_binary_to_oct (a,B,mn)       **********/
+/***********   Description   :  convert bin. into octet    **********/
+/********************************************************************/
+void convert_binary_to_oct(short *A, int64 *B, short mn) {
+    register i, j, k;
+
+    i = mn * OCT - 1;
+    for (k = 0; k <= mn - 1; k++) {
+        B[k] = 0x00;
+        for (j = 7; j >= 0; j--) {
+            B[k] += A[i--] * o_mask[j];
+            if (i < 0) break;
+        }
+        if (i < 0) break;
+    }
+}
+
+/********************************************************************/
+/***********   Function name :  convert_oct_to_binary (A,b,mn)       **********/
+/***********   Description   :  convert octet into bin.    **********/
+/********************************************************************/
+void convert_oct_to_binary(int64 *A, short *B, short mn) {
+    register i, j, k;
+
+    for (i = 0; i < mn * OCT; i++) B[i] = 0;
+    k = mn * OCT - 1;
+    for (i = 0; i <= mn - 1; i++) {
+        for (j = 0; j <= 7; j++) {
+            B[k--] = (A[i] >> j) & 0x01;
+            if (k < 0) break;
+        }
+        if (k < 0) break;
+    }
+}
+
+/********************************************************************/
+/*****     Function name : WM_Left_Pow(A,E,C,N,mn)              *****/
+/*****     Description   : C= A^E mod N                         *****/
+/********************************************************************/
+void left_to_right_pow(int64 *A, int64 *E, int64 *C, int64 *N, short mn) {
+    register i;
+    int64 arryC[mb] = {
+        0,
+    };
+    short e[m] = {
+        0,
+    };
+
+    for (i = 0; i < mn; i++)
+        C[i] = 0x00;
+
+    convert_radix_to_binary(E, e, mn);
+
+    arryC[0] = 0x01;
+
+    for (i = 0; i < mn * DHEX; i++) {
+        convert_mma(arryC, arryC, arryC, N, mn);
+
+        if (e[i] == 1)
+            convert_mma(arryC, A, arryC, N, mn);
+    }
+
+    for (i = 0; i < mn; i++)
+        C[i] = arryC[i];
+}
